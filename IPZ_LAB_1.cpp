ba2eecd8b3caca4e249e9e11b3cf66e7ea443f7a
BOBYCH ANDREW <65415696+qawsed281113@users.noreply.github.com>
2024-09-18 22:25:03 +0300

    Initial commit

---
+++ IPZ_LAB_1.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int fibonacci(int n) {
+    if (n <= 1) {
+        return n;
+    }
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+int main() {
+    int n;
+    cout << "Enter index of Fibonachi: ";
+    cin >> n;
+
+    cout << "Fibonachi for index :  " << n << ": " << fibonacci(n) << endl;
+
+    return 0;
+}
